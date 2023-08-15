e1e7243293e30f0c3553e30756a69c682085e111
themaomao <117374532+themaomao@users.noreply.github.com>
2023-08-15 10:56:51 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Main from './components/Main';
+import './components/style.css'
+
+function App() {
+  
+  return (
+    <>
+    <Main />
+    </>
+  );
+}
+
+export default App;
